71ab92f09f01a444e7d4ae63e331119573e3c655
Kurt Johnson <fazbat@gmail.com>
2017-12-11 14:47:57 -0800

    modified server to handle client-side routing in production(2)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   //app.use(express.static(path.join(__dirname, 'client/build')));
   app.use(express.static('client/build'));//TODO
 
-  app.get('/', function (req, res) {
+  app.get('/*', function (req, res) {
     console.log("call to home>>>>>>>>>>>>",__dirname);
     res.sendFile(path.join(__dirname,'client/build', 'index.html'));
   });
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log(">>>(process.env.NODE_ENV !== 'production'");
   app.use(express.static(path.join(__dirname, 'client/public')));
 
-  app.get('/*', function (req, res) {
+  app.get('/', function (req, res) {
     console.log("call to home");
     res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
   });
